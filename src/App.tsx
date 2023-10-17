6c51bc16223babd8e2d77f997012b555c078c012
Lucas <lucasguarnelli@hotmail.com>
2023-10-17 06:09:05 -0300

    feat: drafting more visuals to Player page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './styles/global.css'
 
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './store'
-import { Player} from '../pages/Player'
+import { Player} from './pages/Player'
 
 export function App() {
   return (
